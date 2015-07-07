fe4b948eca7f1209f59beea3fec7e8edd28a0d07
Francisco Souza <f@souza.cc>
2015-07-06 23:33:02 -0300

    db: add function for connecting to the database

---
+++ db.go
@@ -0,0 +1,23 @@
+// Copyright 2015 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"gopkg.in/mgo.v2"
+)
+
+type collection struct {
+	*mgo.Collection
+	*mgo.Session
+}
+
+func connect() (*collection, error) {
+	session, err := mgo.DialWithTimeout(config.MongoURL, 30e9)
+	if err != nil {
+		return nil, err
+	}
+	coll := session.DB(config.DBName).C("instances")
+	return &collection{coll, session}, nil
+}
---
+++ db_test.go
@@ -0,0 +1,19 @@
+// Copyright 2015 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"gopkg.in/check.v1"
+)
+
+func (*S) TestConnect(c *check.C) {
+	config.MongoURL = "mongodb://127.0.0.1:27017/diaats"
+	config.DBName = "diaats"
+	coll, err := connect()
+	c.Assert(err, check.IsNil)
+	defer coll.Close()
+	c.Assert(coll.Database.Name, check.Equals, config.DBName)
+	c.Assert(coll.Name, check.Equals, "instances")
+}
