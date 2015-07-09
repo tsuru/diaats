46d414483a889758ca7c6abc8011042141dd07f9
Francisco Souza <f@souza.cc>
2015-07-09 00:24:19 -0300

    api: implement function for generating muxer from API
    Also adding all the handlers.

--- api.go
+++ api.go
@@ -5,7 +5,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
+
+	"github.com/gorilla/pat"
 )
 
 func handler(fn http.HandlerFunc) http.HandlerFunc {
@@ -19,6 +25,95 @@ func handler(fn http.HandlerFunc) http.HandlerFunc {
 		}
 		fn.ServeHTTP(w, r)
 	})
+}
+
+func createInstance(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	if name == "" {
+		http.Error(w, "please provide the name of the instance", http.StatusBadRequest)
+		return
+	}
+	planName := r.FormValue("plan")
+	if planName == "" {
+		http.Error(w, "please provide the name of the plan", http.StatusBadRequest)
+		return
+	}
+	plan, err := getPlan(planName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = CreateInstance(name, plan)
+	if err != nil {
+		status := http.StatusInternalServerError
+		if err == ErrInstanceAlreadyExists {
+			status = http.StatusConflict
+		}
+		http.Error(w, err.Error(), status)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func bindApp(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get(":name")
+	instance, err := GetInstance(name)
+	if err != nil {
+		status := http.StatusInternalServerError
+		if err == ErrInstanceNotFound {
+			status = http.StatusNotFound
+		}
+		http.Error(w, err.Error(), status)
+		return
+	}
+	encodedEndpoints, _ := json.Marshal(instance.Endpoints())
+	envVarName := fmt.Sprintf("DIAATS_%s_INSTANCE", strings.ToUpper(instance.Plan.Name))
+	data := map[string]string{envVarName: string(encodedEndpoints)}
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		log.Printf("ERROR - failed to encode JSON: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func removeInstance(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get(":name")
+	err := DestroyInstance(name)
+	if err != nil {
+		status := http.StatusInternalServerError
+		if err == ErrInstanceNotFound {
+			status = http.StatusNotFound
+		}
+		http.Error(w, err.Error(), status)
+		return
+	}
+}
+
+func instanceStatus(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get(":name")
+	_, err := GetInstance(name)
+	if err != nil {
+		status := http.StatusInternalServerError
+		if err == ErrInstanceNotFound {
+			status = http.StatusNotFound
+		}
+		http.Error(w, err.Error(), status)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func buildMuxer() http.Handler {
+	m := pat.New()
+	m.Post("/resources/{name}/bind-app", handler(bindApp))
+	m.Delete("/resources/{name}/bind-app", handler(func(http.ResponseWriter, *http.Request) {}))
+	m.Post("/resources/{name}/bind", handler(func(http.ResponseWriter, *http.Request) {}))
+	m.Delete("/resources/{name}/bind", handler(func(http.ResponseWriter, *http.Request) {}))
+	m.Get("/resources/{name}/status", handler(instanceStatus))
+	m.Delete("/resources/{name}", handler(removeInstance))
+	m.Post("/resources", handler(createInstance))
+	return m
 }
 
 func main() {
--- api_test.go
+++ api_test.go
@@ -5,10 +5,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/fsouza/go-dockerclient"
 	dtesting "github.com/fsouza/go-dockerclient/testing"
 	"gopkg.in/check.v1"
 )
@@ -25,11 +28,16 @@ type S struct {
 
 func (s *S) SetUpTest(c *check.C) {
 	var err error
+	config.Username = ""
+	config.Password = ""
 	config.MongoURL = "mongodb://127.0.0.1:27017/diaats"
 	config.DBName = "diaats"
 	s.server, err = dtesting.NewServer("127.0.0.1:0", nil, nil)
 	c.Assert(err, check.IsNil)
 	config.DockerHost = s.server.URL()
+	coll, err := connect()
+	c.Assert(err, check.IsNil)
+	coll.RemoveAll(nil)
 }
 
 func (s *S) TearDownTest(c *check.C) {
@@ -113,4 +121,147 @@ func (*S) TestHandlerWrongCredentials(c *check.C) {
 	h.ServeHTTP(recorder, request)
 	c.Assert(recorder.Code, check.Equals, http.StatusUnauthorized)
 	c.Assert(called, check.Equals, false)
+}
+
+func (*S) TestCreateInstanceHandler(c *check.C) {
+	client, err := docker.NewClient(config.DockerHost)
+	c.Assert(err, check.IsNil)
+	err = client.PullImage(docker.PullImageOptions{Repository: "memcached"}, docker.AuthConfiguration{})
+	c.Assert(err, check.IsNil)
+	config.Plans = []Plan{{Name: "supermemcached", Image: "memcached"}}
+	body := strings.NewReader("name=mycache&plan=supermemcached")
+	request, err := http.NewRequest("POST", "/resources", body)
+	c.Assert(err, check.IsNil)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusCreated)
+	defer DestroyInstance("mycache")
+	_, err = GetInstance("mycache")
+	c.Assert(err, check.IsNil)
+}
+
+func (*S) TestCreateInstanceHandlerDuplicate(c *check.C) {
+	client, err := docker.NewClient(config.DockerHost)
+	c.Assert(err, check.IsNil)
+	err = client.PullImage(docker.PullImageOptions{Repository: "memcached"}, docker.AuthConfiguration{})
+	c.Assert(err, check.IsNil)
+	config.Plans = []Plan{{Name: "supermemcached", Image: "memcached"}}
+	err = CreateInstance("mycache", &config.Plans[0])
+	c.Assert(err, check.IsNil)
+	defer DestroyInstance("mycache")
+	body := strings.NewReader("name=mycache&plan=supermemcached")
+	request, err := http.NewRequest("POST", "/resources", body)
+	c.Assert(err, check.IsNil)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusConflict)
+}
+
+func (*S) TestCreateInstanceHandlerInvalidPlan(c *check.C) {
+	client, err := docker.NewClient(config.DockerHost)
+	c.Assert(err, check.IsNil)
+	err = client.PullImage(docker.PullImageOptions{Repository: "memcached"}, docker.AuthConfiguration{})
+	c.Assert(err, check.IsNil)
+	config.Plans = []Plan{{Name: "supermemcached", Image: "memcached"}}
+	body := strings.NewReader("name=mycache&plan=hipermemcached")
+	request, err := http.NewRequest("POST", "/resources", body)
+	c.Assert(err, check.IsNil)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusBadRequest)
+	c.Assert(recorder.Body.String(), check.Equals, "plan not found\n")
+}
+
+func (*S) TestBindAppHandler(c *check.C) {
+	client, err := docker.NewClient(config.DockerHost)
+	c.Assert(err, check.IsNil)
+	err = client.PullImage(docker.PullImageOptions{Repository: "memcached"}, docker.AuthConfiguration{})
+	c.Assert(err, check.IsNil)
+	config.Plans = []Plan{{Name: "supermemcached", Image: "memcached"}}
+	err = CreateInstance("mycache", &config.Plans[0])
+	c.Assert(err, check.IsNil)
+	defer DestroyInstance("mycache")
+	request, err := http.NewRequest("POST", "/resources/mycache/bind-app", strings.NewReader(""))
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Log(recorder.Body.String())
+	c.Assert(recorder.Code, check.Equals, http.StatusOK)
+	expected := map[string]string{"DIAATS_SUPERMEMCACHED_INSTANCE": "[]"}
+	var result map[string]string
+	err = json.Unmarshal(recorder.Body.Bytes(), &result)
+	c.Assert(err, check.IsNil)
+	c.Assert(result, check.DeepEquals, expected)
+}
+
+func (*S) TestBindAppHandlerNotFound(c *check.C) {
+	request, err := http.NewRequest("POST", "/resources/mycache/bind-app", strings.NewReader(""))
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Log(recorder.Body.String())
+	c.Assert(recorder.Code, check.Equals, http.StatusNotFound)
+}
+
+func (*S) TestRemoveInstanceHandler(c *check.C) {
+	client, err := docker.NewClient(config.DockerHost)
+	c.Assert(err, check.IsNil)
+	err = client.PullImage(docker.PullImageOptions{Repository: "memcached"}, docker.AuthConfiguration{})
+	c.Assert(err, check.IsNil)
+	config.Plans = []Plan{{Name: "supermemcached", Image: "memcached"}}
+	err = CreateInstance("mycache", &config.Plans[0])
+	c.Assert(err, check.IsNil)
+	defer DestroyInstance("mycache")
+	request, err := http.NewRequest("DELETE", "/resources/mycache", strings.NewReader(""))
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusOK)
+	_, err = GetInstance("mycache")
+	c.Assert(err, check.Equals, ErrInstanceNotFound)
+}
+
+func (*S) TestRemoveInstanceHandlerNotFound(c *check.C) {
+	request, err := http.NewRequest("DELETE", "/resources/mycache", strings.NewReader(""))
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Log(recorder.Body.String())
+	c.Assert(recorder.Code, check.Equals, http.StatusNotFound)
+}
+
+func (*S) TestInstanceStatusHandler(c *check.C) {
+	client, err := docker.NewClient(config.DockerHost)
+	c.Assert(err, check.IsNil)
+	err = client.PullImage(docker.PullImageOptions{Repository: "memcached"}, docker.AuthConfiguration{})
+	c.Assert(err, check.IsNil)
+	config.Plans = []Plan{{Name: "supermemcached", Image: "memcached"}}
+	err = CreateInstance("mycache", &config.Plans[0])
+	c.Assert(err, check.IsNil)
+	defer DestroyInstance("mycache")
+	request, err := http.NewRequest("GET", "/resources/mycache/status", strings.NewReader(""))
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusNoContent)
+}
+
+func (*S) TestInstanceStatusHandlerNotFound(c *check.C) {
+	request, err := http.NewRequest("GET", "/resources/mycache/status", strings.NewReader(""))
+	c.Assert(err, check.IsNil)
+	recorder := httptest.NewRecorder()
+	handler := buildMuxer()
+	handler.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusNotFound)
 }
