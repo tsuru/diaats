dcb5f1e9a0fbacffc600a54ab9ee0daf3116e3db
Francisco Souza <fss@corp.globo.com>
2015-07-31 15:06:59 -0300

    all: extending licensing information
    Change copyright holder and add files listing authors and contributors.

--- api_test.go
+++ api_test.go
@@ -1,4 +1,4 @@
-// Copyright 2015 Francisco Souza. All rights reserved.
+// Copyright 2015 diaats authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
